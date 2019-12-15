5bd0e8c09ceabc669e35355d9043035c3dfb4542
Kha'Lus <56515008+KhaLus0@users.noreply.github.com>
2019-12-15 20:36:40 +0100

    Minor change.

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -120,7 +120,7 @@ class Game {
                     if (command.startsWith("MOVE")) {
                         String[] temp = command.split(" ");
                         move(Integer.parseInt(temp[1]), Integer.parseInt(temp[2]), this);
-                    } else if (command.startsWith("PASS" )) {
+                    } else if (command.startsWith("PASS")) {
                         pass(this);
                     } else if (command.startsWith("RESIGN")) {
                         resign(this);
